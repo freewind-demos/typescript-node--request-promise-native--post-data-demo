1f8d32fd9d4e529167175ebee3e6bfa5504673f6
freewind <nowindlee@gmail.com>
2019-05-06 16:59:07 +0800

    demo

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-TypeScript Node "request-promise-native" Demo
+TypeScript Node "request-promise-native" Post Data Demo
 =============================================
 
-让"request"支持async/await
+发出一个post请求，post data放在`json` field中。
 
 ```
 npm install
--- hello.ts
+++ hello.ts
@@ -1,7 +1,11 @@
 import request from 'request-promise-native';
 
 async function run() {
-  const response = await request("https://postman-echo.com/get?hello=request");
+  const response = await request.post("https://postman-echo.com/post?param1=value1", {
+    json: {
+      "hello": "json-data1"
+    }
+  });
   console.log(typeof response)
   console.log(response)
 }
